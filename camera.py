e53899145af4c0dec1699a9253f477993406ba50
Milind Sharma <46882470+Milind-Sharma@users.noreply.github.com>
2019-05-21 15:17:34 +0530

    Update camera.py

--- camera.py
+++ camera.py
@@ -28,3 +28,36 @@ camera.start_recording('/home/pi/Desktop/Raspberry/videos/videos.h264')
 sleep(5)
 camera.stop_recording()
 camera.stop_preview()
+
+#effects in the camera
+camera.resolution = (2592, 1944)       #maximum resolution
+camera.framerate = 15
+
+camera.start_preview(alpha=200)
+
+camera.annotate_background = Color('grey')      #for text-background color
+camera.annotate_foreground = Color('black')     #for text color
+camera.annotate_text = "Hey There!"             #for printing the text on the photo
+camera.annotate_text_size = 150                 #for text size
+
+sleep(2)
+camera.capture('/home/pi/Desktop/max.jpg')
+camera.stop_preview()
+
+#for image effect
+camera.start_preview(alpha=200)
+camera.image_effect = 'colorswap'
+sleep(2)
+camera.capture('/home/pi/Desktop/hello.jpg')
+camera.stop_preview()
+
+#for different photo effects using loop 
+camera.start_preview(alpha=200)
+for effect in camera.IMAGE_EFFECTS:
+    camera.image_effect = effect
+    camera.annotate_text = "Effect: %s" % effect
+    sleep(2)
+    camera.capture('/home/pi/Desktop/hello1.jpg')
+camera.stop_preview()
+
+
