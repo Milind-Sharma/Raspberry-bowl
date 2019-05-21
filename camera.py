6d9d63697c642a8059dd341def3ee48ab269ead1
Milind Sharma <46882470+Milind-Sharma@users.noreply.github.com>
2019-05-21 12:49:44 +0530

    Update camera.py

--- camera.py
+++ camera.py
@@ -3,7 +3,7 @@ from time import sleep
 
 camera = PiCamera()
 
-camera.rotation = 180               //to rotate the camera if the preview is upside-down
+camera.rotation = 180               #to rotate the camera if the preview is upside-down
 camera.start_preview(alpha=215)
-sleep(10)                           //to turn off the preview after this time
+sleep(10)                           #to turn off the preview after this time
 camera.stop_preview()
